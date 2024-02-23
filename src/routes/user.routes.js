0721f028a621630a485a22fedb0d1cfb7254effd
RohitGoyal <rohitgoyal474@gmail.com>
2024-02-23 23:51:46 +0530

    create usercontroller, update cloudinary,

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,23 @@
 import { Router } from "express";
 import {registerUser} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router=Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    // upload is multer middleware
+    // this store this avatar and cover imgae in public/temp
+    // this will give req.file
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        }, 
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser)
 
 export default router
