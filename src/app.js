91fbe4664312ca23483f906b25f39eeac8dfbd82
RohitGoyal <rohitgoyal474@gmail.com>
2024-03-20 02:36:11 +0530

    fix index.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
-import express from "express";
-const app=express();
-import cors from "cors";
-import cookieParser from "cookie-parser";
-app.use(cors({
-    origin:true,
-    credentials:true
-}));
-app.use(express.json({limit:"16kb"}));
-app.use(express.urlencoded({extended:true}));
-app.use(cookieParser());
-app.use(express.static("public"));
+// import express from "express";
+// const app=express();
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+// app.use(cors({
+//     origin:true,
+//     credentials:true
+// }));
+// app.use(express.json({limit:"16kb"}));
+// app.use(express.urlencoded({extended:true}));
+// app.use(cookieParser());
+// app.use(express.static("public"));
 
-/// router import
-import userRouter from "./routes/user.routes.js";
+// /// router import
+// import userRouter from "./routes/user.routes.js";
 
-// route declaration
+// // route declaration
 
-app.use("/api/vi/users",userRouter);
+// app.use("/api/vi/users",userRouter);
 
     
 
-export{app}
+// export{app}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import connectDB from "./db/index.js"
-import {app} from "./app.js"
+import connectDB from "./db/index.js";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.routes.js";
+const app = express();
+
+await connectDB();
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static("public"));
+
+app.use("/api/vi/users", userRouter);
 
 
-connectDB()
-.then(() => {
-    
-    app.listen(process.env.PORT, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
-    console.log("DB is not connected", err);
-})
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  })
+//   .catch((err) => {
+//     console.log("DB is not connected", err);
+//   });
